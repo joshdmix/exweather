68d048a9745784b27781f85592ff2fb8dc9dd031
Josh Mix <josh.mix@eltoro.com>
2020-11-28 13:03:46 -0500

    response formatted

--- lib/exweather/request.ex
+++ lib/exweather/request.ex
@@ -16,9 +16,9 @@ defmodule Exweather.Request do
   end
 
   def handle_response({:ok, %_{status_code: 200, body: body}}) do
-    Logger.info("successful response")
-    Logger.debug(fn -> inspect(body) end)
-    {:ok, :jsx.decode(body)}
+    formatted_body = format_body(:jsx.decode(body))
+    Logger.info(inspect(formatted_body))
+    {:ok, formatted_body}
   end
 
   def handle_response({:ok, %_{status_code: status, body: body}}) do
@@ -28,5 +28,23 @@ defmodule Exweather.Request do
 
   def owm_url(zip_code) do
     @owm_url <> "zip=#{zip_code}&appid=#{System.get_env("OWM_API_KEY")}"
+  end
+
+  def format_body(body) do
+    body_map = Enum.into(body, %{})
+    main = body_map |> Map.get("main") |> Enum.into(%{})
+    coord = body_map["coord"] |> Enum.into(%{})
+
+    %{
+      city: body_map["name"],
+      lat: coord["lat"],
+      lon: coord["lon"],
+      max: main["temp_max"],
+      min: main["temp_min"],
+      temp: main["temp"],
+      feels_like: main["feels_like"],
+      pressure: main["pressure"],
+      humidity: main["humidity"]
+    }
   end
 end
